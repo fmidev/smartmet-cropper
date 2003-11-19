c6dccfa6872dfa982677a66ee14a7719fff6af50
mheiskan <mheiskan>
2003-11-19 07:21:13 +0000

    1. Muutin virheenkäsittelyn niin, että http-moodissa     vriheet tulostetaan HTTP-koodien kautta. 2. Pelkkä kuvanvälitys ilman croppausta toimii nyt.

--- cropper.cpp
+++ cropper.cpp
@@ -6,6 +6,7 @@
 // ======================================================================
 
 #include "NFmiCmdLine.h"
+#include "NFmiFileSystem.h"
 #include "NFmiStringTools.h"
 
 #include <cstdlib>
@@ -37,13 +38,34 @@ void usage()
 
 // ----------------------------------------------------------------------
 /*!
+ * \brief Output the given imagefile
+ */
+// ----------------------------------------------------------------------
+
+void http_output_image(const string & theFile)
+{
+  ifstream in(theFile.c_str(), ios::in|ios::binary);
+  if(!in)
+	throw runtime_error("Image '"+theFile+"' was lost!");
+
+  cout << "HTTP/1.1 200 OK" << endl
+	   << "Content-Type: image/png" << endl
+	   << "Content-Length: " << NFmiFileSystem::FileSize(theFile) << endl
+	   << endl
+	   << in.rdbuf();
+  in.close();
+}
+
+// ----------------------------------------------------------------------
+/*!
  * \brief The main algorithm
  */
 // ----------------------------------------------------------------------
 
 int domain(int argc, const char * argv[])
 {
-  map<string,string> options;
+  typedef map<string,string> Options;
+  Options options;
 
   if(getenv("QUERYSTRING") != 0)
 	options = NFmiStringTools::ParseQueryString();
@@ -76,8 +98,36 @@ int domain(int argc, const char * argv[])
 
 	}
 
-  
-  
+  const Options::const_iterator end = options.end();
+
+  const bool has_option_f = (options.find("f") != end);
+  const bool has_option_p = (options.find("p") != end);
+  const bool has_option_c = (options.find("c") != end);
+  const bool has_option_n = (options.find("n") != end);
+  const bool has_option_o = (options.find("o") != end);
+
+  if(!has_option_f)
+	throw runtime_error("Must give image name to be cropped");
+
+  if(has_option_p + has_option_c + has_option_n > 1)
+	throw runtime_error("Too many cropping geometries defined, use only one");
+
+  // Check the image exists
+
+  const string imagefile = options.find("f")->second;
+  if(!NFmiFileSystem::FileExists(imagefile))
+	throw runtime_error("The desired image '"+imagefile+"' does not exist");
+
+  // Quick special case
+
+  if(has_option_p + has_option_c + has_option_n == 0)
+	{
+	  if(has_option_o)
+		NFmiFileSystem::CopyFile(imagefile,options.find("o")->second);
+	  else
+		http_output_image(imagefile);
+	  return 0;
+	}
 
   return 0;
 
@@ -91,18 +141,42 @@ int domain(int argc, const char * argv[])
 
 int main(int argc, const char * argv[])
 {
+  const bool httpmode = (getenv("QUERYSTRING") != 0);
+
   try
 	{
 	  return domain(argc, argv);
 	}
+
   catch(exception & e)
 	{
-	  cerr << "Error: Caught an exception" << endl
-		   << " --> " << e.what() << endl;
+	  if(!httpmode)
+		{
+		  cerr << "Error: Caught an exception" << endl
+			   << " --> " << e.what() << endl;
+		}
+	  else
+		{
+		  cout << "HTTP/1.1 409 Conflict" << endl
+			   << "Content-Type: text/html" << endl
+			   << endl
+			   << e.what()
+			   << endl;
+		}
 	}
+
   catch(...)
 	{
-	  cerr << "Error: Caught an unknown exception" << endl;
+	  if(!httpmode)
+		cerr << "Error: Caught an unknown exception" << endl;
+	  else
+		{
+		  cout << "HTTP/1.1 409 Conflict" << endl
+			   << "Content-Type: text/html" << endl
+			   << endl
+			   << "Unknown exception occurred"
+			   << endl;
+		}
 	}
   return 1;
 }
